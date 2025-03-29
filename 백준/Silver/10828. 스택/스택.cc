4feca6d78f7b6a3e0228fa1a435c6a8cc4d2dde8
Damin <153018626+kdm0927@users.noreply.github.com>
2025-03-29 13:05:37 +0900

    [Silver IV] Title: 스택, Time: 220 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,45 +1,55 @@
 #include <iostream>
 #include <stack>
 #include <string>
+
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;  // 명령의 수
+    int n;
+    int x = 0;
+    
+    stack <int> s;
+    cin >> n;
 
-    stack<int> s;  // 스택을 구현하기 위한 스택 변수
-
-    for (int i = 0; i < N; ++i) {
-        string command;
-        cin >> command;  // 명령 읽기
-
-        if (command == "push") {
-            int X;
-            cin >> X;
-            s.push(X);  // 스택에 X를 넣음
+    for (int i=0; i<n; i++) {
+        string str;
+        cin >> str;
+        //push
+        if (str=="push") { //입력값이 push이면
+            cin >> x;
+            s.push(x);
         }
-        else if (command == "pop") {
+        //top
+        else if (str=="top") {
             if (s.empty()) {
                 cout << -1 << endl;  // 스택이 비어 있으면 -1 출력
             } else {
                 cout << s.top() << endl;  // 스택의 가장 위 값을 출력
-                s.pop();  // 스택에서 가장 위 값을 제거
             }
         }
-        else if (command == "size") {
+        //pop
+        else if (str=="pop") {
+            if (s.empty()==false) {
+                cout << s.top() << endl;
+                s.pop();   
+            }
+            else if (s.empty()==true) {
+                cout << -1 << endl;
+            }
+        }
+        //empty
+        else if (str=="empty") {
+            if (s.empty()==false) {
+                cout << 0 << endl;
+            }
+            else if (s.empty()==true) {
+                cout << 1 << endl;
+            }
+        }
+        //size
+        else if (str == "size") {
             cout << s.size() << endl;  // 스택에 있는 원소의 개수 출력
         }
-        else if (command == "empty") {
-            cout << (s.empty() ? 1 : 0) << endl;  // 스택이 비어 있으면 1, 아니면 0 출력
-        }
-        else if (command == "top") {
-            if (s.empty()) {
-                cout << -1 << endl;  // 스택이 비어 있으면 -1 출력
-            } else {
-                cout << s.top() << endl;  // 스택의 가장 위 값을 출력
-            }
-        }
     }
-
     return 0;
 }
